978a26ed4ab2d4cb89339a16439f5a4d8d02f56b
water quality map <76960481+waterqualitymap@users.noreply.github.com>
2021-01-04 19:19:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ export function getMap(position, tooltip) {
   // добавляем маркер с сообщением
   L.marker(position).addTo(map).bindPopup(tooltip).openPopup()
 }
+
+// находим кнопку и добавляем к ней обработчик
+document.getElementById('my_position').onclick = () => {
+  navigator.geolocation.getCurrentPosition(success, error, {
+    enableHighAccuracy: true
+  })
+}
+
+function success({ coords }) {
+  const { latitude, longitude } = coords
+  const currentPosition = [latitude, longitude]
+  // вызываем функцию, передавая ей текущую позицию и сообщение
+  getMap(currentPosition, 'You are here')
+}
+
+function error({ message }) {
+  console.log(message)
+}
